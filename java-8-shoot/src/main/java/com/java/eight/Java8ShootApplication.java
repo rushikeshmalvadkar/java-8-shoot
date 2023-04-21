7dd08880a8812b3c062b6b74d0faca370923cf9d
abhishek malvadkar ranoji <abhishek@dev.com>
2023-04-21 17:01:07 +0530

    Created spring boot project for practice

---
+++ java-8-shoot/src/main/java/com/java/eight/Java8ShootApplication.java
@@ -0,0 +1,13 @@
+package com.java.eight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8ShootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8ShootApplication.class, args);
+	}
+
+}
---
+++ java-8-shoot/src/test/java/com/java/eight/Java8ShootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.eight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8ShootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
