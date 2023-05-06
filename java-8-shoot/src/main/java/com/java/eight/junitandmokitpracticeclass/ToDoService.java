4682fe19478b1558751fc22991b19d729966168f
abhishek malvadkar ranoji <abhishek@dev.com>
2023-05-06 11:58:38 +0530

    learn Mocking

---
+++ java-8-shoot/src/main/java/com/java/eight/junitandmokitpracticeclass/ToDoService.java
@@ -0,0 +1,7 @@
+package com.java.eight.junitandmokitpracticeclass;
+
+import java.util.List;
+
+public interface ToDoService {
+	public List<String> getTodos(String user);  
+}
---
+++ java-8-shoot/src/test/java/com/java/eight/junitandmokitpracticeclass/ToDoBusinessTest.java
@@ -0,0 +1,38 @@
+package com.java.eight.junitandmokitpracticeclass;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class ToDoBusinessTest {
+
+	@Test
+	void getTodosforSpringTestNoSoringContains() {
+		ToDoService toDoServiceMock = Mockito.mock(ToDoService.class);
+		
+		List<String> todos = Arrays.asList("work","eat");
+		Mockito.when(toDoServiceMock.getTodos("rushikesh")).thenReturn(todos);
+		
+		ToDoBusiness toDoBusiness = new ToDoBusiness(toDoServiceMock);
+		List<String> todosforSpring = toDoBusiness.getTodosforSpring("rushikesh");
+		assertEquals(0, todosforSpring.size());
+	}
+	@Test
+	void getTodosforSpringTestIfContainsSpring() {
+		ToDoService toDoServiceMock = Mockito.mock(ToDoService.class);
+		
+		List<String> todos = Arrays.asList("Spring","eat");
+		Mockito.when(toDoServiceMock.getTodos("rushikesh")).thenReturn(todos);
+		
+		ToDoBusiness toDoBusiness = new ToDoBusiness(toDoServiceMock);
+		List<String> todosforSpring = toDoBusiness.getTodosforSpring("rushikesh");
+		assertEquals("Spring", todosforSpring.get(0));
+	}
+	
+
+}
